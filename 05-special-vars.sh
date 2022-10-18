8686e7eea70379a44d2e7460ef2ab4e3a395f6aa
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 08:53:38 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -4,4 +4,8 @@
 
 echo "Name of the executed script is: $0"       # Prints the script name that you're running
 echo "First Argument from command line: $1"
-echo "Second Argument from command line: $1"
+echo "Second Argument from command line: $2"
+
+# On command you can supply 9 variables 
+# Ex  
+#  sh abc.sh 10 20 30 40
