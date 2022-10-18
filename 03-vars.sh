aa876b9210e5cef8298fc7d1b951ea0f24404635
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 08:40:08 +0530

    Auto Push Commit

--- 03-vars.sh
+++ 03-vars.sh
@@ -23,4 +23,4 @@ echo "prining the value of c : $c"
 # rm -rf /data/${OPT_VARIABLE}  # rm -rf /data/
 
 
-# Note: local variables
+# Note: local variables will have higher priority than export variables
