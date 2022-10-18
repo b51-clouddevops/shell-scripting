e07909d6c8923731e425b603c11886c6e4e368a0
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 08:52:58 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -3,3 +3,4 @@
 ### Special Variables in Linux are : $0 to $n , $* , $@ , $# , $$ 
 
 echo "Name of the executed script is: $0"       # Prints the script name that you're running
+echo "First Argument from command line: $1"
