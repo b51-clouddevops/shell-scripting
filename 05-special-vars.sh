c7439bd060503907958580869afc2a14a1669d40
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 09:03:28 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -9,7 +9,7 @@ echo $2   # Second argument from the command line
 echo $*   # Gives you all the arguments used in the script
 echo $@   # Gives you all the arguments used in the script
 echo $#   # Gves you the number of arguments users
-echo $$   # Gives you the PID of the 
+echo $$   # Gives you the PID of the current process
 
 # On command you can supply 9 variables 
 # Ex  
