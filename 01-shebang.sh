78b6e2084a0784bc0c88b85b2a40150fb1c5fb1d
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 07:57:12 +0530

    My first push

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -5,3 +5,10 @@ echo Hello World
 echo line1 
 echo line2
 echo line3
+
+# Special Characters in linux for specific purpose.
+# /n  : cursor moves to next 
+# /t  : cursor moves to tab space 
+# Whenever you want to use special characters, ensure you enbale them with -e option in echo
+
+echo "line1 /n line2"
