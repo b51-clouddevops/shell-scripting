e8db9110f2d2cf5c45f03197e1f9000f6a9c17bb
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 08:05:29 +0530

    My first push

---
+++ 02-color-codes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+# Colors are of 2 types `FOREGROUND` & `BACKGROUND` Color.
+# Colors       Foreground          Background
+
+# Red               31                  41
+# Green             32                  42
+# Yellow            33                  43
+# Blue              34                  44
+# Magenta           35                  45
+# Cyan              36                  46
+# ```
+
+# Syntax : echo -e "\e[COLORCODEm  Your Text \e[0m"
+
+echo -e "\e[33m I am printing Yellow \e[0m"
