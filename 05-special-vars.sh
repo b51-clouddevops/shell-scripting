88d902a2c289c111c4b4ba424cd139f663da99d3
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 09:01:04 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -2,13 +2,13 @@
 
 ### Special Variables in Linux are : $0 to $n , $* , $@ , $# , $$ 
 
-echo -e "\e[32m Name of the executed script is:\e[0m $0"       # Prints the script name that you're running
+echo $0 #Name of the executed script is:\e[0m $0"       # Prints the script name that you're running
 echo "First Argument from command line: $1"
 echo "Second Argument from command line: $2"
 
 echo $*   # Gives you all the arguments used in the script
 echo $@   # Gives you all the arguments used in the script
-echo $#   # Gves you the number of argume
+echo $#   # Gves you the number of arguments users
 
 # On command you can supply 9 variables 
 # Ex  
