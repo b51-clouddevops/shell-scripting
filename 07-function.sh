c6a58ebefa23776956e2d8d6d978e5994e32489b
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 07:57:06 +0530

    Auto Push Commit

--- 07-function.sh
+++ 07-function.sh
@@ -13,6 +13,7 @@ sample
 stat() {
     echo "Load Average on the system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
     echo "Number of logged in sessions is : $(who  |wc -l)"
+    echo ""
 }
 
 stat 
