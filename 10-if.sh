c5630e2586442d205cf7fd56708724848d50fa26
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 08:44:55 +0530

    Auto Push Commit

--- 10-if.sh
+++ 10-if.sh
@@ -30,9 +30,8 @@ elif [ "$c" -eq "20" ] ; then
     echo "value of c is 20"
 
 elif [ "$c" -eq "30" ] ; then 
-    echo "value of c is 30 "
+    echo "value of c is 30"
 
 else 
     echo "Value is not 10 or 20 or 30"
-
 fi 
