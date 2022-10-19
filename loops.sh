e72d9325990302a4964aa2de6e0159877aadeaf1
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 08:51:10 +0530

    Auto Push Commit

--- loops.sh
+++ loops.sh
@@ -2,7 +2,7 @@
 
 # Loops based on inputs. for loop is the option 
 
-for games in cricket volleyball soccer coco chess; do  
+for i in cricket volleyball soccer coco chess; do  
     echo Game Name is ${games}
 done 
 
