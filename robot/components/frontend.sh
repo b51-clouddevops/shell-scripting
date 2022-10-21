19dcdf147f77e95cb36de4c8c426cb2bd0cda95f
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:24:14 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -25,7 +25,7 @@ else
     echo -e "\e[31m Failure \e[0m"
 fi 
 
-echo "Performing Cleanup"
+echo -n "Performing Cleanup"
 rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then 
     echo -e "\e[32m Success \e[0m"
