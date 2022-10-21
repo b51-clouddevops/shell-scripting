6173f7a6b3f8ab13c36211c510c39d65d7237eae
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:18:01 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -13,16 +13,16 @@ yum install nginx -y &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
 
 
 # What I am planning to have ?
