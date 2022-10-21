1cce7ec4ade5a453d88ac5a01bf7de9f14351e25
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:09:58 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -16,7 +16,7 @@ systemctl enable nginx
 systemctl start nginx 
 
 
-What I am planning to have ?
-1) If any steps fails, I don't want to proceed further and want to break the script. 
-2) I want validation to happen first and then if it's a root or sudo user,then only I want to run. If not, I'd like to break the script.
-3) 
+# What I am planning to have ?
+# 1) If any steps fails, I don't want to proceed further and want to break the script. 
+# 2) I want validation to happen first and then if it's a root or sudo user,then only I want to run. If not, I'd like to break the script.
+# 3) I would like to add Success / Failure messages next to each instruction.
