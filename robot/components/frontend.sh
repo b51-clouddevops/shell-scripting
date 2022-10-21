70bb10a2272fef04e93f3005dbbbd6127f32eade
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:38:16 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -12,7 +12,7 @@ if [ $USERID -ne 0 ]  ; then
 fi 
 
 stat() {
-    if [ $1 -eq 0 ]; then 
+    if [ $? -eq 0 ]; then 
         echo -e "\e[32m Success \e[0m"
     else 
         echo -e "\e[31m Failure \e[0m"
