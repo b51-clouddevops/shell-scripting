fae53f3c8ee8a8651d4f2305cc2acbb5bea62365
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 09:00:35 +0530

    Auto Push Commit

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -16,7 +16,7 @@ yum install mongodb-org -y &>> $LOGFILE
 stat $?
 
 echo -n "Updating the mongodb config:"
-sed -i -e 's/127.0.0.1/0.0.0.0/' mongodb.conf 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf 
 stat $? 
 
 echo -n "Strating MongoDB: "
