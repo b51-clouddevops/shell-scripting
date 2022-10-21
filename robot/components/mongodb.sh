5095f8e44df1ce2ee6ed0c553cfa031838981060
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:47:11 +0530

    Auto Push Commit

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -11,30 +11,6 @@ echo -n "Installing Ngnix:"
 yum install nginx -y &>> $LOGFILE
 stat $?
 
-echo -n "Downloading the component: "
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
-
-echo -n "Performing Cleanup:"
-rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
-stat $?
-
-cd /usr/share/nginx/html
-echo -n "Unzipping the $COMPONENT:"
-unzip /tmp/$COMPONENT.zip &>> $LOGFILE
-stat $? 
-mv $COMPONENT-main/* .
-mv static/* .
-rm -rf $COMPONENT-main README.md &>> $LOGFILE
-
-echo -n "Conifuring the reverse proxy file:"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-stat $?
-
-echo -n "Starting Frontend Service:"
-systemctl enable nginx &>> $LOGFILE
-systemctl start nginx &>> $LOGFILE
-stat $?
 
 
 # What I am planning to have ?
