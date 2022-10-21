fe67f0d4338da15048055adbd9ac6cf49ed4c6ed
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:29:45 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -19,11 +19,11 @@ stat() {
 }
 
 echo -n "Installing Ngnix:"
-yum install nginx -y &>> /tmp/frontend.log
+yum install nginx -y &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "Downloading the component: "
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 echo -n "Performing Cleanup:"
