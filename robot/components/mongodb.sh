03369bd4acce518c5488b890f2a4b5116845fc04
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:39:28 +0530

    Auto Push Commit

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,3 +1,55 @@
 #!/bin/bash 
+set -e 
 
-echo "I am mongodb component"
+# Verify the script is been executed as a root user or not.
+USERID=$(id -u) 
+COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
+
+if [ $USERID -ne 0 ]  ; then 
+    echo -e "\e[31m You must run this script as a root user or with sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+    fi 
+}
+
+echo -n "Installing Ngnix:"
+yum install nginx -y &>> $LOGFILE
+stat $?
+
+echo -n "Downloading the component: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo -n "Performing Cleanup:"
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
+stat $?
+
+cd /usr/share/nginx/html
+echo -n "Unzipping the $COMPONENT:"
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $? 
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md &>> $LOGFILE
+
+echo -n "Conifuring the reverse proxy file:"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Starting Frontend Service:"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
+
+
+# What I am planning to have ?
+# 1) If any steps fails, I don't want to proceed further and want to break the script. 
+# 2) I want validation to happen first and then if it's a root or sudo user,then only I want to run. If not, I'd like to break the script.
+# 3) I would like to add Success / Failure messages next to each instruction.
