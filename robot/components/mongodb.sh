6189286b2967b74b184d5c211572fa689f77ae63
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 09:04:19 +0530

    Auto Push Commit

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -34,16 +34,3 @@ cd mongodb-main
 mongo < catalogue.js &>> $LOGFILE
 mongo < users.js &>> $LOGFILE
 stat $?
-
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
-
-# What I am planning to have ?
-# 1) If any steps fails, I don't want to proceed further and want to break the script. 
-# 2) I want validation to happen first and then if it's a root or sudo user,then only I want to run. If not, I'd like to break the script.
-# 3) I would like to add Success / Failure messages next to each instruction.
