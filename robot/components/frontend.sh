fa594f7164c3135dcd6baad0cb76e1400e66afec
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:07:09 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -15,11 +15,3 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx 
 systemctl start nginx 
 
-
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
