1b4ea5f3579f5e45826501ec221facfc83d45b22
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:04:06 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
 yum install nginx -y 
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
 systemctl enable nginx 
 systemctl start nginx 
