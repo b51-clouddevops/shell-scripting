7030a639802125aea40887e928b38eea227cd25c
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 08:41:14 +0530

    Auto Push Commit

---
+++ robot/components/common.sh
@@ -0,0 +1,14 @@
+LOGFILE=/tmp/$COMPONENT.log
+
+if [ $USERID -ne 0 ]  ; then 
+    echo -e "\e[31m You must run this script as a root user or with sudo privilege \e[0m"
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+    fi 
+}
