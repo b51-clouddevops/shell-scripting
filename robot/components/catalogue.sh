d9c89fdb1f96d4ed95cbd5affd1be8d04dcb1b99
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 08:30:29 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -50,7 +50,10 @@ stat $?
 
 echo -n "Starting $COMPONENT Service:"
 systemctl daemon-reload &>> $LOGFILE
-systemctl start $COMPONENT 
+systemctl start $COMPONENT &>> $LOGFILE
+stat $? 
+
+echo -n ""
 
 
 # 1. Update SystemD file with correct IP addresses
