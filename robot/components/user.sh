839fca0657e39aa643e3181e2633c66073fbf361
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 08:51:09 +0530

    Auto Push Commit

---
+++ robot/components/user.sh
@@ -0,0 +1,73 @@
+#!/bin/bash 
+
+COMPONENT=user
+
+source components/common.sh
+APPUSER=roboshop
+
+echo -n "Configuring Node JS:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>> $LOGFILE
+stat $? 
+
+echo -n "Installing Nodejs: "
+yum install nodejs -y &>> $LOGFILE
+stat $? 
+
+id $APPUSER &>> $LOGFILE 
+if [ $? -ne 0 ]; then
+    echo -n "Creating App User:"
+    useradd $APPUSER &>> $LOGFILE
+    stat $?
+fi 
+
+echo -n "Downloading the $COMPONENT:"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $? 
+
+echo -n "Moving $COMPONENT Code to $APPUSER home directory:"
+cd /home/$APPUSER/ 
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
+stat $? 
+
+echo -n "Performing Clearnup:"
+rm -rf $COMPONENT
+mv $COMPONENT-main $COMPONENT
+stat $? 
+
+echo -n "Installing nodejs dependencies:"
+cd $COMPONENT 
+npm install  &>> $LOGFILE
+stat $?
+
+echo -n "Changing permissions to $APPUSER"
+chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT &&  chmod -R 775 /home/roboshop/$COMPONENT 
+stat $?
+
+echo -n "Configuring $COMPONENT Service:"
+sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+stat $? 
+
+echo -n "Starting $COMPONENT Service:"
+systemctl daemon-reload &>> $LOGFILE
+systemctl start $COMPONENT &>> $LOGFILE
+stat $? 
+
+echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
+
+
+# 1. Update SystemD file with correct IP addresses
+    
+#     Update `MONGO_DNSNAME` with MongoDB Server IP
+
+# # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# # systemctl daemon-reload
+# # systemctl start catalogue
+# # systemctl enable catalogue
+# # systemctl status catalogue -l
+
+# NOTE: You should see the log saying `connected to MongoDB`, then only your catalogue
+# will work and can fetch the items from MongoDB
+
+# Ref Log:
+# {"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":"MongoDB connected","v":1}
