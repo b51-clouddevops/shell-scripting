8cbb900e74df7c8a1c3e31eb0aa056dd35b99305
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 08:43:57 +0530

    Auto Push Commit

---
+++ robot/components/redis.sh
@@ -0,0 +1,10 @@
+# curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+# yum install redis-6.2.7 -y
+
+# vim /etc/redis.conf
+# vim /etc/redis/redis.conf
+
+```sql
+# vim /etc/redis.conf
+# vim /etc/redis/redis.conf
+```
