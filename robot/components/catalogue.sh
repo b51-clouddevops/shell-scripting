078247e9e7370f171c855675818a7a696d983470
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 07:47:31 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -14,3 +14,18 @@ echo -n "Installing Nodejs: "
 # curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 # yum install nodejs -y
 
+# useradd roboshop
+
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+$ cd /home/roboshop
+$ unzip /tmp/catalogue.zip
+$ mv catalogue-main catalogue
+$ cd /home/roboshop/catalogue
+$ npm install
+
+
+1. Update SystemD file with correct IP addresses
+    
+    Update `MONGO_DNSNAME` with MongoDB Server IP
+
+    
