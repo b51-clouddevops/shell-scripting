9c55cbc817b12a4796d03f308a256f83cb4936b4
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 08:45:49 +0530

    Auto Push Commit

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -10,13 +10,10 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.r
 stat $? 
 
 echo -n "Installing $COMPONENT:"
-yum install redis-6.2.7 -y 
+yum install redis-6.2.7 -y  &>> $LOGFILE
 
-# curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-# yum install redis-6.2.7 -y
 
-# vim /etc/redis.conf
-# vim /etc/redis/redis.conf
+
 
 ```sql
 # vim /etc/redis.conf
