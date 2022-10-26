82b7dcbbdbcf2fefb8b4acba884d596201cd22c2
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 22:53:53 +0530

    cart

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -8,6 +8,8 @@ echo -n "Configuring the $COMPONENT Repo:"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE 
 stat $? 
 
+echo "MySQL Password is $MYSQL_PWD ...."
+
 echo -n "Installing $COMPONENT:"
 yum install mysql-community-server -y &>> $LOGFILE 
 stat $? 
