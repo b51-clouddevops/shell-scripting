f3487f60ff6190a69461c78b06751efc76fa2291
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 08:32:40 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -8,3 +8,4 @@ echo -n "Configuring the MySql Repo:"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE 
 stat $? 
 
+echo -n "Installing My"
