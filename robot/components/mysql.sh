c4bc24e6324fad6d66bb6e5b7d474fdf04e680f0
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 09:08:14 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -22,17 +22,17 @@ DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' '
 # I only want to change the default password only for the first time.
 # How do I come to know whether the password is changed or not.
 
-echo show databases | mysql -uroot -p${SQL_PASSWORD} &>> $LOGFILE 
+echo show databases | mysql -uroot -p${1} &>> $LOGFILE 
 if [ $? -ne 0 ] ; then 
     echo -n "Reset Root Password"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${1}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
     stat $? 
 fi 
 
 echo show plugins | mysql -uroot -p${SQL_PASSWORD} | grep validate_password; &>> $LOGFILE 
 if [ $? -eq 0 ] ; then 
     echo -n "Uninstalling Password Validate Plugin: "
-    echo "uninstall plugin validate_password;" | mysql  --connect-expired-password  -uroot -p${SQL_PASSWORD} &>> $LOGFILE 
+    echo "uninstall plugin validate_password;" | mysql  --connect-expired-password  -uroot -p${1} &>> $LOGFILE 
     stat $? 
 fi 
 
