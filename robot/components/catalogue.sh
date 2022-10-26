6f41b04922ee3d810fe6bc38176163399e9d192c
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 08:14:41 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -7,14 +7,14 @@ APPUSER=roboshop
 
 NODEJS
 
-echo -n "Configuring $COMPONENT Service:"
-sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $? 
+# echo -n "Configuring $COMPONENT Service:"
+# sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
+# mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+# stat $? 
 
-echo -n "Starting $COMPONENT Service:"
-systemctl daemon-reload &>> $LOGFILE
-systemctl start $COMPONENT &>> $LOGFILE
-stat $? 
+# echo -n "Starting $COMPONENT Service:"
+# systemctl daemon-reload &>> $LOGFILE
+# systemctl start $COMPONENT &>> $LOGFILE
+# stat $? 
 
 echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
