e97127e1f4d698bc4b2d7795c7c3fced593c7fac
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 08:11:54 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -8,28 +8,10 @@ APPUSER=roboshop
 NODEJS
 
 
-# echo -n "Downloading the $COMPONENT:"
-# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-# stat $? 
-
-# echo -n "Moving $COMPONENT Code to $APPUSER home directory:"
-# cd /home/$APPUSER/ 
-# unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-# stat $? 
-
-# echo -n "Performing Cleanup:"
-# rm -rf $COMPONENT
-# mv $COMPONENT-main $COMPONENT
-# stat $? 
-
 echo -n "Installing nodejs dependencies:"
 cd $COMPONENT 
 npm install  &>> $LOGFILE
 stat $?
-
-# echo -n "Changing permissions to $APPUSER"
-# chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT &&  chmod -R 775 /home/roboshop/$COMPONENT 
-# stat $?
 
 echo -n "Configuring $COMPONENT Service:"
 sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/' /home/roboshop/$COMPONENT/systemd.service
