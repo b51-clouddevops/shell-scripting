abe3f3675647b00a3ac6e5d29895a6fdd296439c
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 08:51:55 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -19,7 +19,8 @@ stat $?
 echo -n "Changing the default password:"
 DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
 
-
+#I only want to change the default password only for the first time.
+# 
 
 echo -n "Reset Root Password"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
