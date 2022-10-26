e1e23bc01f9de6db9fd458b6579bdb78e00de760
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 09:06:22 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -22,10 +22,10 @@ DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' '
 # I only want to change the default password only for the first time.
 # How do I come to know whether the password is changed or not.
 
-echo show databases | mysql -uroot -p${MYSQL_P} &>> $LOGFILE 
+echo show databases | mysql -uroot -p${SQL_PASSWORD} &>> $LOGFILE 
 if [ $? -ne 0 ] ; then 
     echo -n "Reset Root Password"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
     stat $? 
 fi 
 
