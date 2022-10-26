fb2666c454c9acb6efb6d5c01f370564befa1e9e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 08:54:51 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -30,7 +30,7 @@ if [ $? -ne 0 ] ; then
 fi 
 
 echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password; &>> $LOGFILE 
-if [ $? -ne 0 ] ; then 
+if [ $? -eq 0 ] ; then 
     echo -n "Reset Root Password"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
     stat $? 
