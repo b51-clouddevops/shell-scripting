3bf407f118a32712d142dc14705ef5d9de41ac60
mvpc-11232 <mvp2612@hotmail.com>
2022-10-27 08:13:36 +0530

    Auto Push Commit

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -4,7 +4,8 @@ COMPONENT=rabbitmq
 source components/common.sh
 
 echo -n "Installing and configuring $COMPONENT repo"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE 
+
 
 
 echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
