351df2cd60fdd26113c4ac98c529ac15efb27f99
mvpc-11232 <mvp2612@hotmail.com>
2022-10-27 08:14:02 +0530

    Auto Push Commit

--- robot/components/rabbitmq.sh
+++ robot/components/rabbitmq.sh
@@ -6,6 +6,10 @@ source components/common.sh
 echo -n "Installing and configuring $COMPONENT repo"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> $LOGFILE 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>> $LOGFILE 
+stat $? 
+
+echo -n "Installing $COMPONENT : "
+
 
 
 echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
