4ee590dac8ba17d6b9e3cd082f94ba0bec890eb4
mvpc-11232 <mvp2612@hotmail.com>
2022-10-29 08:01:06 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -32,6 +32,7 @@ stat $?
 echo -n "Configuring Reverse Proxy : "
 sed -e "/user/s/localhost/user.robot.internal/"  /etc/nginx/default.d/roboshop.conf 
 
+for component in a b c d e; do
 echo -n "Starting Frontend Service:"
 systemctl enable nginx &>> $LOGFILE
 systemctl start nginx &>> $LOGFILE
