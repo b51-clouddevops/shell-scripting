feb846fe9d6160c12e89fe271a2a0fe420840463
mvpc-11232 <mvp2612@hotmail.com>
2022-10-29 07:48:56 +0530

    Auto Push Commit

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -71,6 +71,7 @@ PYTHON() {
     USERID=$(id -u roboshop)
     GROUPID=$(id -g roboshop)
 
+    cd /home/$APPUSER/$COMPONENT/
     pip3 install -r requirements.txt
     
     echo -n "Updating the uid and gid with $APPUSER in $PAYMENT.ini : "
