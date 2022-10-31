20a28269c56c7e22f2995879ceb0f901f471bb18
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:24:16 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 if [ -z "$1" ]; then 
-    echo -e "\e[31m Component name is required \n example usage is: \n\t bash create-server.sh componentName"    
+    echo -e "\e[31m Component name is required \n example usage is: \n\t bash create-server.sh componentName \e[0m"    
 fi 
 
 COMPONENT=$1
