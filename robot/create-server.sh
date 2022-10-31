f3b59e84ce0d57856447ee7dcebfab6223899779
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:18:24 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -5,5 +5,5 @@ COMPONENT=$1
 AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq  '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
-echo "SG ID Used to launch instance is : $SG_ID"
+echo "SG ID Used to launch instance is : $SGID"
 aws ec2 run-instances  --image-id $AMI_ID  --instance-type t2.micro --security-group-ids $SGID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
