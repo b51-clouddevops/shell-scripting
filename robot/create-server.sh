f453ef69dc591f2666f4039998578cafb92f60e1
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:21:08 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -6,6 +6,7 @@ AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Valu
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
+echo $COMPONENT
 aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
 
 Error parsing parameter '--tag-specifications': Expected: ',', received: ']' for input:
