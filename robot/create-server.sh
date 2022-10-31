135e69ccadc595a43e865dea56e231deacfd8f1b
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:09:01 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -5,6 +5,3 @@ AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Valu
 echo $AMI_ID
 
 aws ec2 run-instances  --image-id $AMI_ID  --instance-type t2.micro | jq 
-   
-   
-   
