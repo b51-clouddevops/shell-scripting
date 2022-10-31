e2f2400115f1576fb16bcec2e0cdbd5389734503
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:13:12 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq  '.Images[].ImageId' | sed -e 's/"//g')"
-SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId'"
+SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' |sed -e 's/"//g')"
 "b51-allow-all"
 echo $AMI_ID
 echo $SG_ID # b51-allow-all
