088ab0248534fc5833dcacd35e1037662ed64377
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:23:14 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
-
 if [ -z "$1" ]; then 
-    
+        
 fi 
+
+COMPONENT=$1
 
 AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq  '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
