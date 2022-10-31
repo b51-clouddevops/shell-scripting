621d7dde63d573edb451f1fca398a3bbe99b6256
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:10:47 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -3,5 +3,5 @@
 AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq  '.Images[].ImageId' | sed -e 's/"//g')"
 
 echo $AMI_ID
-
+echo $SG_ID $ b51-allow-all
 aws ec2 run-instances  --image-id $AMI_ID  --instance-type t2.micro | jq 
