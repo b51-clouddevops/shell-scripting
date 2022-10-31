292379f0a2146e3da0e0e4051ba30324b58e4c96
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:04:31 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -3,3 +3,8 @@
 AMI_ID="$(aws ec2 describe-images  --region us-east-1  --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq  '.Images[].ImageId' | sed -e 's/"//g')"
 
 echo $AMI_ID
+
+aws ec2 run-instances  --image-id ami-0abcdef1234567890 \
+   
+   
+    --key-name MyKeyPair
