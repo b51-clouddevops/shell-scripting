7b98fcd86cbfb55f1ad71f1223b493e09d7299e6
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:42:12 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -12,5 +12,5 @@ SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq )
 
