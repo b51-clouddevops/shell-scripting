321a76c15f19433a4223a973f87352ce7683b784
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:43:29 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -14,4 +14,4 @@ echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress')
 
-sed -e "s/"
+sed -e "s/IPADDRESS/"
