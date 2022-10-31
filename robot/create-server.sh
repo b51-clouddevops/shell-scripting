21e39a6083fe68ad995dac56b3b687f30d557d57
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 08:21:46 +0530

    Auto Push Commit

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -9,5 +9,3 @@ echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
 aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
 
-Error parsing parameter '--tag-specifications': Expected: ',', received: ']' for input:
-ResourceType=instance,Tags=[{Key=Name,Value=}]
